da3b8f3725bd5dbdfd3b0114aabed37c9c27d0f2
Dindumahesh <maheshdindu996@gamil.com>
2026-06-28 10:46:35 +0530

    first commit

---
+++ expression.py/leet.py/formulas.p/expression.py
@@ -0,0 +1,135 @@
+# all expression formules
+ 1. Addition
+   a + b
+
+2. Subtraction
+   a - b
+
+3. Multiplication
+   a * b
+
+4. Division
+   a / b
+
+5. Floor Division
+   a // b
+
+6. Modulus (Remainder)
+   a % b
+
+7. Exponent (Power)
+   a ** b
+
+8. Average
+   (a + b) / 2
+
+9. Square
+   a ** 2
+
+10. Cube
+    a ** 3
+
+11. Square Root
+    a ** 0.5
+
+12. Cube Root
+    a ** (1/3)
+
+13. Absolute Value
+    abs(a)
+
+14. Maximum
+    max(a, b)
+
+15. Minimum
+    min(a, b)
+
+16. Square of Sum
+    (a + b) ** 2
+
+17. Square of Difference
+    (a - b) ** 2
+
+18. Cube of Sum
+    (a + b) ** 3
+
+19. Cube of Difference
+    (a - b) ** 3
+
+20. Sum of Squares
+    a ** 2 + b ** 2
+
+21. Difference of Squares
+    a ** 2 - b ** 2
+
+22. Sum of Cubes
+    a ** 3 + b ** 3
+
+23. Product
+    a * b
+
+24. Quotient
+    a / b
+
+25. Percentage
+    (obtained / total) * 100
+
+26. Simple Interest
+    (P * T * R) / 100
+
+27. Compound Interest
+    P * (1 + R/100) ** T
+
+28. Area of Rectangle
+    l * b
+
+29. Perimeter of Rectangle
+    2 * (l + b)
+
+30. Area of Square
+    s ** 2
+
+31. Perimeter of Square
+    4 * s
+
+32. Area of Triangle
+    (b * h) / 2
+
+33. Area of Circle
+    3.14 * r ** 2
+
+34. Circumference of Circle
+    2 * 3.14 * r
+
+35. Volume of Cube
+    s ** 3
+
+36. Volume of Cuboid
+    l * b * h
+
+37. Volume of Cylinder
+    3.14 * r ** 2 * h
+
+38. Even Check
+    a % 2 == 0
+
+39. Odd Check
+    a % 2 != 0
+
+40. Greater Than
+    a > b
+
+41. Less Than
+    a < b
+
+42. Equal To
+    a == b
+
+43. Not Equal To
+    a != b
+
+44. Greater Than or Equal To
+    a >= b
+
+45. Less Than or Equal To
+    a <= b
